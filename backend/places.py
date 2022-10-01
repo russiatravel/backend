91e2d6c1404c1cac7137bb43c205c672412535d0
Art Gilyazov <art.gilyazov@gmail.com>
2022-10-01 15:35:38 +0300

    add class storage

--- backend/places.py
+++ backend/places.py
@@ -1,33 +1,55 @@
 from flask import Flask, request
+from typing import Any
 
 app = Flask(__name__)
 
-places = []
+Json = dict[int, Any]
+class Storage:
+    def __init__(self):
+
+        self.places: dict[int, Json] = {}
+        self.last_uid = 0
+
+    def add(self, place: Json) -> Json:
+        self.last_uid += 1
+        place["uid"] = self.last_uid
+        self.places[self.last_uid] = place
+        return place
+
+    def get_all(self) -> list[Json]:
+        return list(self.places.values())
+
+    def get_by_id(self, uid:int) -> Json:
+        return self.places[uid]
+
+    def update(self, uid: int, place: Json) -> Json:
+        self.places[uid] = place
+        return place
+
+    def delete(self, uid: int) -> None:
+        self.places.pop(uid)
+
+storage = Storage()
 
 @app.post('/api/places/')
 def add():
-    payload = request.json
-    payload["uid"] = len(places)
-    places.append(payload)
-    return payload, 201
+    place = request.json
+    return storage.add(place), 201
 
 @app.get('/api/places/')
 def get_all():
-    return places, 200
+    return storage.get_all(), 200
 
 @app.get('/api/places/<int:uid>')
 def get_by_id(uid):
-    return places[uid], 200
+    return storage.get_by_id(uid), 200
 
 @app.put('/api/places/<int:uid>')
 def update_by_id(uid):
-    payload = request.json
-
-    for key in payload:
-        places[uid][key] = payload[key]
-
-    return places[uid]
+    place = request.json
+    return storage.update(uid, place), 200
 
 @app.delete('/api/places/<int:uid>')
 def delete_place(uid):
-    return places.pop(uid), 201
+    storage.delete(uid)
+    return {}, 204
