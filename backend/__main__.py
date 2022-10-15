8c335f7abe47d3c405151c30a25bd3d34224cf3a
guryanovaa56 <andreyg56@yandex.ru>
2022-10-15 15:00:33 +0500

    Chore: add shutdown session

--- backend/__main__.py
+++ backend/__main__.py
@@ -2,7 +2,7 @@ import logging
 
 from flask import Flask
 from pydantic import ValidationError
-
+from backend.database import db_session
 from backend.errors import AppError
 from backend.places.views import place_view
 from backend.cities.views import city_view
@@ -11,8 +11,13 @@ app = Flask(__name__)
 app.register_blueprint(place_view, url_prefix='/api/places')
 app.register_blueprint(city_view, url_prefix='/api/cities')
 
+
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+
+
+def shutdown_session(exception=None):
+    db_session.remove()
 
 
 def handle_app_error(err: AppError):
@@ -25,6 +30,7 @@ def handle_validation_error(err: ValidationError):
 
 app.register_error_handler(AppError, handle_app_error)
 app.register_error_handler(ValidationError, handle_validation_error)
+app.teardown_appcontext(shutdown_session)
 
 
 def main():
