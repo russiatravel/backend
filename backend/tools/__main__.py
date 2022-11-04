97e11c72600cb8f0f0b0ab39e093fd33f4cd7b3f
guryanovaa56 <andreyg56@yandex.ru>
2022-11-04 14:11:01 +0500

    optimize by comments

--- backend/tools/__main__.py
+++ backend/tools/__main__.py
@@ -1,14 +1,11 @@
 import csv
-import os
-
+from typing import Any
 from backend.database import db_session
 from backend.models import Place, City
 
-db_data = os.environ['DB_DATA']
 
-
-def read_csv(filename):
-    with open(filename, 'r', encoding= 'utf-8') as f:
+def read_csv(filename: str) -> list[dict[str, Any]]:
+    with open(filename, 'r', encoding='utf-8') as f:
         fields = ['city_name', 'place_name', 'place_description']
         reader = csv.DictReader(f, fields, delimiter=',')
         travels_data = []
@@ -17,40 +14,48 @@ def read_csv(filename):
         return travels_data
 
 
-def save_cities(data):
-    processed = []
-    unique_cities = []
+def get_unique_cities(data):
+    cities = {}
     for row in data:
-        if row['city_name'] not in processed:
-            city = {"name": row["city_name"]}
-            unique_cities.append(city)
-            processed.append(city["name"])
-    db_session.bulk_insert_mappings(City, unique_cities, return_defaults=True)
+        name = row['city_name']
+        if name not in cities:
+            city = {'name': name}
+            cities[name] = city
+
+    return cities
+
+
+def save_cities(cities):
+    db_session.bulk_insert_mappings(City, cities.values(), return_defaults=True)
     db_session.commit()
-    return unique_cities
 
 
-def get_city_by_id(cities, city_name):
-    for city in cities:
-        if city["name"] == city_name:
-            return city["uid"]
+def get_unique_places(data, cities):
+    places = {}
 
-
-def save_places(data, cities):
-    processed = []
-    unique_places = []
     for row in data:
-        if row["place_name"] not in processed:
-            place = {"name": row["place_name"], "description": row["place_description"],
-            "city_id": get_city_by_id(cities, row["city_name"])}
-            unique_places.append(place)
-            processed.append(place["name"])
-    db_session.bulk_insert_mappings(Place, unique_places, return_defaults=True)
+        city_name = row['city_name']
+        place_name = row['place_name']
+        if place_name not in places:
+            place = {
+                "name": place_name,
+                "description": row["place_description"],
+                "city_id": cities[city_name]['uid']
+            }
+            places[place_name] = place
+
+    return places
+
+
+def save_places(places):
+    db_session.bulk_insert_mappings(Place, places.values(), return_defaults=True)
     db_session.commit()
-    return unique_places
 
 
 if __name__ == '__main__':
-    all_data = read_csv(db_data)
-    cities = save_cities(all_data)
-    places = save_places(all_data, cities)
+    all_data = read_csv('.data/result.csv')
+    cities = get_unique_cities(all_data)
+    save_cities(cities)
+
+    places = get_unique_places(all_data, cities)
+    save_places(places)
